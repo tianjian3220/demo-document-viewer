ce2526cf224b9ca0e20897c0aac145c9143a2500
Lu <zijun.lu@t-systems.com>
2018-12-03 11:31:43 +0800

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private platform: Platform,
+    private document: DocumentViewer) {
 
+  }
+
+  ionViewDidLoad() {
+    if(this.platform.is('cordova')) {
+      const options: DocumentViewerOptions = {
+        title: 'My PDF'
+      }
+  
+      this.document.viewDocument('assets/test.pdf', 'application/pdf', options)
+    }
+    
   }
 
 }
